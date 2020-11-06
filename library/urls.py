19a8d2a7a9bb5eb2a75d1c92a8964b137927fa7a
José Hildebrando <jose.hildebrando@gmail.com>
2020-11-06 11:04:49 -0300

    image upload

--- library/urls.py
+++ library/urls.py
@@ -19,9 +19,13 @@ from django.urls import path, include
 from rest_framework import routers
 from books.api import viewsets as booksviewsets
 route = routers.DefaultRouter()
-route.register(r'books/',booksviewsets.BooksViewSet,  basename="Books")
+route.register(r'books',booksviewsets.BooksViewSet,  basename="Books")
+
+# Imports for image
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
